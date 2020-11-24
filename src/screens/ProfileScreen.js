9010b5a9b6537c4d2c928fdf00c8680a0301aab5
Daniel Cambronero <cambroes@gmail.com>
2020-11-23 19:50:56 -0600

    primer commit

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+export const ProfileScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Profile</Text>
+        </SafeAreaView>
+    );
+};
+
---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+module.exports = {
+	SCREEN: {
+		MAIN: 'Main',
+		PROFILE: 'Profile',
+		HOME: 'Home',
+		DETAILS: 'Details',
+		CASTANDCREW: 'CastAndCrew',
+    },
+	API_KEY: '69146f6f49cb168131caf595beb9774e',
+	COLORS: {
+		WARNING: '#FFBC03',
+		LIGHT_GRAY: '#D7D7D7',
+		LIGHT_GRAY2: '#F5F5F5',
+		GRAY: '#ACAEAD',
+		GRAY2: '#ACAEADAA',
+		LIGHT: '#FFFFFF',
+		PRIMARY: '#5FB1A2',
+		PRIMARY2: '#5FB1A25A',
+		TEXT_COLOR: '#313234',
+		TEXT_COLOR2: '#AFB0B0',
+	},
+};
